ed83535bd7f38d294fb5ed02ebde74197c3cb2a3
mhayesCS <97618624+mhayesCS@users.noreply.github.com>
2023-03-14 11:46:05 -0400

    LinkedList Progression

--- src/main/java/com/github/curriculeon/MyLinkedList.java
+++ src/main/java/com/github/curriculeon/MyLinkedList.java
@@ -16,17 +16,21 @@ public class MyLinkedList<SomeType> implements MyCollectionInterface<SomeType>{
     }
     @Override
     public void add(SomeType objectToAdd) {
+        // declare the type of object and name and set it to a new node
         MyNode<SomeType> nodeToAdd = new MyNode<>();
         nodeToAdd.setData(objectToAdd);
+        // if there is no objects in the head node then add a new one
         if (headNode == null) {
             headNode = nodeToAdd;
             return;
 
         }
+        // while the current node is empty get the next node
         MyNode<SomeType> current = headNode;
         while (current != null) {
             MyNode<SomeType> nextNode = current.getNext();
             boolean isNextNodeNull = nextNode == null;
+            // if there is nothing inside of the nextNode then add a node
             if (isNextNodeNull) {
                 current.setNext(nodeToAdd);
                 break;
